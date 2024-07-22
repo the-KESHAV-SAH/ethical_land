d8b91a75e442cf44f8d922bc9fe9b92cc69b2278
the-KESHAV-SAH <97468880+the-KESHAV-SAH@users.noreply.github.com>
2024-07-22 20:40:15 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: 'https://ethical-land.netlify.app/', // your frontend domain
+    origin: 'https://ethical-land.netlify.app', // your frontend domain
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // if you are using cookies
